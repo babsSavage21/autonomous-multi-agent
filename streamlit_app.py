f7750ef81dfec851862da81b38b994ccf5e3e2c0
Prajjal Dasverma <p.dasverma@tcs.com>
2025-03-28 23:59:45 +0530

    updated streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,7 @@ from crewai_tools import (
 )
 from crewai.tools import tool
 from textwrap import dedent
-from langchain_ibm import WatsonxLLM
 from ibm_watson_machine_learning.foundation_models import Model
-from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
-from ibm_watsonx_ai.foundation_models.utils.enums import DecodingMethods
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -58,139 +55,119 @@ custom_headers = {
     )
 }
 
-@tool("FetchProductReviewTool")
-def fetch_product_review_tool():
-    """ Description """
-    def get_soup(url):
-        response = requests.get(url, headers=custom_headers)
+def get_soup(url):
+    response = requests.get(url, headers=custom_headers)
 
-        if response.status_code != 200:
-            exit(-1)
+    if response.status_code != 200:
+        print("Error in getting webpage")
+        exit(-1)
 
-        return BeautifulSoup(response.text, "lxml")
+    return BeautifulSoup(response.text, "lxml")
     
-    def extract_review(review, is_local=True):
-        author = review.select_one(".a-profile-name").text.strip()
-        rating = (
-            review.select_one(".review-rating > span").text
-            .replace("out of 5 stars", "")
-            .strip()
-        )
-        date = review.select_one(".review-date").text.strip()
-        
-        if is_local:
-            title = (
-                review.select_one(".review-title")
-                .select_one("span:not([class])")
-                .text.strip()
-            )
-            content = ' '.join(
-                review.select_one(".review-text").stripped_strings
-            )
-            img_selector = ".review-image-tile"
-        else:
-            title = (
-                review.select_one(".review-title")
-                .select_one("span:not([class])").title
-            )
-            content = ' '.join(
-                review.select_one(".review-text").stripped_strings
-            )
-            img_selector = ".review-image-tile"
-        
-        verified_element = review.select_one("span.a-size-mini")
-        verified = verified_element.text.strip() if verified_element else None
-
-        image_elements = review.select(img_selector)
-        images = (
-            [img.attrs["data-src"] for img in image_elements] 
-            if image_elements else None
-        )
-
-        return {
-            "type": "local" if is_local else "global",
-            "author": author,
-            "rating": rating,
-            "title": title,
-            "content": content.replace("Read more", ""),
-            "date": date,
-            "verified": verified,
-            "images": images
-        }
+def extract_review(review, is_local=True):
+    author = review.select_one(".a-profile-name").text.strip()
+    rating = (
+        review.select_one(".review-rating > span").text
+        .replace("out of 5 stars", "")
+        .strip()
+    )
+    date = review.select_one(".review-date").text.strip()
     
-    def get_reviews(soup):
-        reviews = []
-        
-        # Get both local and global reviews using the same function.
-        local_reviews = soup.select("#cm-cr-dp-review-list > li")
-        global_reviews = soup.select("#cm-cr-global-review-list > li")
-        
-        for review in local_reviews:
-            reviews.append(extract_review(review, is_local=True))
-        
-        for review in global_reviews:
-            reviews.append(extract_review(review, is_local=False))
-        return reviews
+    if is_local:
+        title = (
+            review.select_one(".review-title")
+            .select_one("span:not([class])")
+            .text.strip()
+        )
+        content = ' '.join(
+            review.select_one(".review-text").stripped_strings
+        )
+        img_selector = ".review-image-tile"
+    else:
+        title = (
+            review.select_one(".review-title")
+            .select_one("span:not([class])").title
+        )
+        content = ' '.join(
+            review.select_one(".review-text").stripped_strings
+        )
+        img_selector = ".review-image-tile"
+    
+    verified_element = review.select_one("span.a-size-mini")
+    verified = verified_element.text.strip() if verified_element else None
 
-    #asin = "B098FKXT8L"
+    image_elements = review.select(img_selector)
+    images = (
+        [img.attrs["data-src"] for img in image_elements] 
+        if image_elements else None
+    )
+
+    return {
+        "type": "local" if is_local else "global",
+        "author": author,
+        "rating": rating,
+        "title": title,
+        "content": content.replace("Read more", ""),
+        "date": date,
+        "verified": verified,
+        "images": images
+    }
+
+def get_reviews(soup):
+    reviews = []
+    
+    local_reviews = soup.select("#cm-cr-dp-review-list > li")
+    global_reviews = soup.select("#cm-cr-global-review-list > li")
+    
+    for review in local_reviews:
+        reviews.append(extract_review(review, is_local=True))
+    
+    for review in global_reviews:
+        reviews.append(extract_review(review, is_local=False))
+    return reviews
+
+def main(search_url):
     review_content= []
-    search_url = f"https://www.amazon.in/dp/{asin}/"
     soup = get_soup(search_url)
     title_element = soup.select_one('#productTitle')
     title = title_element.text.strip()
     
-    rating_element = soup.select_one('#acrPopover')
-    rating_text = rating_element.attrs.get('title')
-    rating = rating_text.replace('out of 5 stars','')
-
     description = soup.select_one(
     '#productDescription, #feature-bullets > ul').text.strip()
 
     reviews = get_reviews(soup)
     for review in reviews:
         review_content.append(review['content'])
+    print("Review Content: ",review_content)
     final_json = {
         "productname":title,
         "productdescription":description,
         "productreviews":review_content
     }
+    print("final_json : ",final_json)
     df = pd.DataFrame(final_json)
 
     return(df)
 
-@tool("FileReadTool")
-def file_read_tool():
-    """Description"""
-    dir1=os.getcwd()
-    # Instantiate tools
-    file_path = os.path.join(dir1 ,'data', 'logs_new.txt')
-    file_read_tool = FileReadTool(file_path=file_path)
-    return file_read_tool.run()
-
 def extract_json_string(text):
-    # Define the regex pattern to match the string between ```JSON and ```
     pattern = re.compile(r'```JSON(.*?)```', re.DOTALL | re.IGNORECASE)
-
-    # Search for the pattern in the text
     match = pattern.search(text)
-
     if match:
-        # Extract and return the matched string
         return match.group(1).strip()
     else:
-        return None
+        return text
 
 
 def get_agent_prompt_input(inputData):
     prompt_input = f"""You are an autonomous AI application. Based on the company profile and task define the worker agent with its role, goal and backstory in JSON format. Generate only the JSON output. Do not provide any explanation or notes.
 
 {{
-"worker_agent": {{
-"name": "",
-"role": "",
-"goal": "",
-"backstory": ""
-}}
+  "worker_agent": {{
+    "name": "",
+    "role": "",
+    "goal": "",
+    "backstory": ""
+  }}
 }}
 
 Input: {inputData}
@@ -198,55 +175,33 @@ Output:"""
     return prompt_input
 
 def get_task_prompt_input(inputData):
+    prompt_input  = f"""Based on the worker agent and task details define the task description and expected output. The expected output should also define the format of the output which is expected. Identify whether the agent is self-sufficient to execute the task or it needs external tools. The list of 5 available tools and their description are mentioned below. Only select a tool if it is necessary to execute the task and the task cannot be done by LLM. For some tasks more than on tool might be needed to execute the task.
 
-    prompt_input  = f"""You are an autonomous AI application. Based on the worker agent and task details define the task description, expected output and the tool(s) which will be required to execute the task. The expected output should also define the format of the output which is expected.
+1. VisionTool: This tool is used to extract text from images. When passed to the agent it will extract the text from the image and then use it to generate a response, report or any other output. The URL or the PATH of the image should be passed to the Agent.
+2. ClassificationTool: The ClassificationTool is designed to classify given data based on their classes.
+3. FetchProductRivewTool: A custom tool designed to extract the product details like name, description and customer review comments from a specified URL of a website.
+4. FileReadTool: The FileReadTool conceptually represents a suite of functionalities within the crewai_tools package aimed at facilitating file reading and content retrieval. This suite includes tools for processing batch text files, reading runtime configuration files, and importing data for analytics. It supports a variety of text-based file formats such as .txt, .csv, .json, and more. Depending on the file type, the suite offers specialized functionality, such as converting JSON content into a Python dictionary for ease of use.
+5. LinkupSearchTool: Tool Description: The LinkupSearchTool provides the ability to query the Linkup API for contextual information and retrieve structured results. This tool is ideal for enriching workflows with up-to-date and reliable information from Linkup, allowing agents to access relevant data during their tasks.
 
 Generate the response in the given JSON format. Generate only the JSON output. Do not provide any additional texts like explanation or notes.
 
 ```JSON
 {{
-"task_description": "",
-"expected_output": "",
-"tool_names": ["", "", ""]
+	"task_description": "",
+	"expected_output": "",
+	"tool_names": ["", ""]
 }}
 ```
-
-The list of available tools and their description are mentioned below. Only select a tool if it is necessary to execute the task and the task cannot be done by LLM. For some tasks more than on tool might be needed to execute the task.
-
-[
-{{
-    "tool_name": "VisionTool",
-    "tool_description": "This tool is used to extract text from images. When passed to the agent it will extract the text from the image and then use it to generate a response, report or any other output. The URL or the PATH of the image should be passed to the Agent."
-}},
-{{
-    "tool_name": "SummarizerTool",
-    "tool_description": "The SummarizerTool is designed to summarize given texts."
-}},
-{{
-    "tool_name": "FetchProductRivewTool",
-    "tool_description": "A custom tool designed to extract the product details like name, description and customer review comments from a specified URL of a website."
-}},
-{{
-"tool_name": "FileReadTool",
-    "tool_description": "The FileReadTool conceptually represents a suite of functionalities within the crewai_tools package aimed at facilitating file reading and content retrieval. This suite includes tools for processing batch text files, reading runtime configuration files, and importing data for analytics. It supports a variety of text-based file formats such as .txt, .csv, .json, and more. Depending on the file type, the suite offers specialized functionality, such as converting JSON content into a Python dictionary for ease of use."
-}},
-{{
-    "tool_name": "LinkupSearchTool",
-    "tool_description": "The LinkupSearchTool provides the ability to query the Linkup API for contextual information and retrieve structured results. This tool is ideal for enriching workflows with up-to-date and reliable information from Linkup, allowing agents to access relevant data during their tasks."
-}}
-]
-
 
 Input: {inputData}
 Output:"""
     return prompt_input
 
-def master_agent_prompt_output(companyProfile, BusinessRule): 
-
+def master_agent_prompt_output(companyProfile, BusinessRule):
     ma_prompt_input1 = "##Company Profile##\n" +  companyProfile + "\n\n##Business Rule##\n" + BusinessRule       
-    prompt_input = f"""You are an autonomous AI application. Based on the company profile and business rules identify the worker agents, task, task description including the business conditions and expected output. The output should be in below JSON format with the worker agents, task description and expected output for each agent.
+    prompt_input = f"""You are an autonomous AI application. Based on the company profile and business rules identify the worker agents, task, task description including the business conditions and expected output. The output should be in below JSON format with the name, task, task description and task output for each agent. All the fields in the JSON are mandatory.
 
-```json
+```JSON
 {{
 "worker_agents": [
     {{
@@ -295,8 +250,7 @@ Output:"""
 
     return final_ma_resp_json
 
-def worker_agents_prompt_output(companyProfile, BusinessRule, ma_result):
-    
+def worker_agents_prompt_output(companyProfile, BusinessRule, ma_result):    
     final_agent_result = []
     final_agent_config = ''
 
@@ -317,7 +271,6 @@ def worker_agents_prompt_output(companyProfile, BusinessRule, ma_result):
         agent_role = agent_result_json['worker_agent']['role']
         agent_goal = agent_result_json['worker_agent']['goal']
         final_agent_config += agent_name.upper() + "\n\n" + "Role: " + agent_role + "\n\n" + "Goal: " + agent_goal + "\n\n\n"
-
 
         final_agent_result.append(agent_result)
 
@@ -349,12 +302,12 @@ def task_prompt_output(companyProfile, ma_result, agent_result):
         task_prompt_input1 = "##Company Profile##\n" +  companyProfile  + "\n\n##Worker Agent##\n" + name + "\n\n##Worker Agent Role##\n"  + role + "\n\n##Worker Agent Goal##\n" + goal + "\n\n##Worker Agent Backstory##\n" + backstory + "\n\n##Task Name##\n" + task_name + "\n\n##Task Description##\n" + task_description
 
         task_prompt_input = get_task_prompt_input(task_prompt_input1)
+        model_id = "meta-llama/llama-3-2-90b-vision-instruct"
         model = get_llm_models(model_id, parameters, project_id)
         task_generated_response = model.generate_text(prompt=task_prompt_input)
         task_generated_response = extract_json_string(task_generated_response)
         task_result = (task_generated_response+" ")[:task_generated_response.find("Input:")]
         task_result_json = json.loads(task_result)
-
         task_desc = task_result_json['task_description']
         task_exp_output = task_result_json['expected_output']
         final_task_config += role.upper() + "\n\n" + "Description: " + "\n" + task_desc + "\n\n" + "Expected Output: " + "\n" + str(task_exp_output) + "\n\n\n\n"
@@ -362,7 +315,7 @@ def task_prompt_output(companyProfile, ma_result, agent_result):
         final_task_result.append(task_result)
         
     task_resp_json = {
-            "type":"actor_task",
+            "type":"Task",
             "value":"The Master Agent assigned the following task(s):" + "\n\n" + final_task_config,
             "taskResult": final_task_result
 
@@ -400,20 +353,32 @@ def multi_agent_crew(max_iter, maResult, agentResult, taskResult, userQuery):
 
         task_result = taskResult
         currentTaks=[]
-
+        file_data = ''
         for idx, currTask in enumerate(task_result):
             task_info = json.loads(currTask.strip())
+            task_tool = task_info['tool_names']
+            
+            for tool_name in task_tool:
+                print(tool_name)
+                print(type(tool_name))
+                if tool_name=='FileReadTool':
+                    file_read_tool = FileReadTool(file_path=userQuery)
+                    file_data = file_read_tool.run()
+
+                elif tool_name=='FetchProductRivewTool':
+                    file_data = main(userQuery)
+            
 
             description = dedent(f"""
                 {task_info['task_description']}
                 
-                Data: {userQuery}
+                Data: {file_data}
             """)
+            
             agent = currentAgents[idx]
             currentTaks.append(Task(
             description=description,
             agent=agent,
-            tool = [fetch_product_review_tool, file_read_tool],
             context=[],
             expected_output=str(task_info['expected_output']),
         ))
@@ -465,13 +430,39 @@ b) Generate 3 answer variations for the each of the 5 generated questions provid
 
 4. After execution of the test scenarios in step 2, do a semantic similarity of the answers generated for each test scenario with the ground truth answers generated in step 1. Based on the semantic similarity score mark the test scenarios as PASS or FAIL assuming the threshold value of 0.7.
         '''
-        inputData = ''        
+        inputData = './test_data/content.txt'        
         return ucName, businessProfile, businessRules, inputData
     else :
-        ucName = ''
-        businessProfile= ''
-        businessRules = ''
-        inputData = ''
+        ucName = 'Product Review System'
+        businessProfile= 'A new age e-commerse serving the needs of today’s smart and value seeking customers. It offers a one-stop shopping experience by offering fresh produce, bakery, dairy products, home and personal care products, general merchandise, smart apparels and appliances, making it a complete shopping destination.'
+        businessRules = '''
+1. Analyze the reviews given by customers and classify each of the review into the following categories:
+Positive
+Neutral
+Negative
+
+2. From the positive reviews generate the positive review summary.
+
+3. From the negative reviews generate the negative review summary.
+
+4. From the customer reviews, for each review extract product features mentioned in that review and classify the sentiment expressed by the customer for that feature. For example:
+Review
+These are the best dairy free creamers that I have tried! Perfect amount of sweet and creamy. I can finally enjoy coffee again. Now the only issue is finding it!
+Features - Sentiment
+sweet and creamy (Taste) - Positive
+availability (Product Availability) - Negative
+
+5. From the customer reviews generate a summarized review.
+
+6. Use the positive review summary from step 2 to generate the Search Engine Optimization content consisting of the following:
+Product description
+H1 headings
+Broad Match Keywords
+Meta Title
+Meta Description
+X (Twitter) Text
+'''
+        inputData = 'https://www.amazon.in/dp/B07MXXW3RL'
         return ucName, businessProfile, businessRules, inputData
 
 @st.dialog("Select")
@@ -483,7 +474,7 @@ def automultiagentselect():
             st.session_state.automultiagentselect = {"selected": "Example1"}
             st.rerun()
     with example2Col:
-        if st.button("Example2", type="primary"):
+        if st.button("Product Review System", type="primary"):
             st.session_state.automultiagentselect = {"selected": "Example2"}
             st.rerun()
     with newRunCol:
