61b6e04e37a53f1b03aeae3a53ce527534541dfe
Prajjal Dasverma <p.dasverma@tcs.com>
2025-03-28 18:46:56 +0530

    test deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from agentFramework import agentFramework
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 usecaseName = st.text_input("Usecase Name", "Enter the name of the use case")
 businessProfile = st.text_area("Business Profile", "Enter the detailed business profile")
